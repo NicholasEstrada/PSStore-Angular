fd899f3ef9a9bc2d5e1990d623fa3fce04508c1e
nicholasestrada <nicholas26cervatti@gmail.com>
2023-10-16 16:34:50 -0300

    primeiro commite da recriação da ps-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
